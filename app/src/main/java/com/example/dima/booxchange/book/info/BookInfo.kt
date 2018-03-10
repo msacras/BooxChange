97dcbed6732cd4a3315f337900702eefc25f6555
Dima Cazacu <dima.cazacu.95@mail.ru>
2018-03-10 14:01:27 +0200

    Started 'BookInfo' activity

---
+++ app/src/main/java/com/example/dima/booxchange/book/info/BookInfo.kt
@@ -0,0 +1,13 @@
+package com.example.dima.booxchange.book.info
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dima.booxchange.R
+
+class BookInfo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_info)
+    }
+}
