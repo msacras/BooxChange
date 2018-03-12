7dfd88b2f799078caa931354fb904cbdd3e279fe
Kristian Velinchuk <cristian.velinciuc@hotmail.com>
2018-03-12 23:59:41 +0200

    Removed migration debris

--- app/src/main/java/com/example/dima/booxchange/screens/LibraryActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.dima.booxchange.screens
-
-import com.example.dima.booxchange.utilities.BaseActivity
-
-/**
- * Created by Dima on 3/10/2018.
- */
-class LibraryActivity: BaseActivity() {
-}
